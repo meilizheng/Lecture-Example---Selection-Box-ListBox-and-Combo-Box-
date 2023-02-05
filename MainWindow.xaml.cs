90592deca12643a8b02b4c4e65c5798bb4df3a29
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-02-05 13:45:43 -0800

    updated code
    updated code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//Meili Zheng;
+//Lecture Example Selection Box
+//02/05/2023
 
 namespace Lecture_Example___Selection_Box___ListBox_and_Combo_Box__
 {
@@ -20,9 +23,124 @@ namespace Lecture_Example___Selection_Box___ListBox_and_Combo_Box__
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students = new List<Student>();
+        Student selectedsudent = null;
+
         public MainWindow()
         {
+           
             InitializeComponent();
+            // how to add items to a selection box;
+            // Add the selection by ACCESS its items list;
+            Preload();
+            DisplayToListBox();
+            DisplayToComboBox();
+            List<string> name = new List<string>();
+            name.Clear();
+            //Automatically select the first item in our listbox on load;
+            
+            lbDisplay.SelectedIndex = 0;
+            cbDisplay.SelectedIndex = 0;
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string UserInput = txtAddToList.Text;
+            lbDisplay.Items.Add(UserInput);
+        }
+
+        public void DisplayToListBox()
+        {
+            //Use the .Clear() method to clear all item from our list box;
+            lbDisplay.Items.Clear();
+            for (int i = 0; i < students.Count; i++)
+            {
+                lbDisplay.Items.Add(students[i]);
+            }
+        }
+        public void DisplayToComboBox()
+        {
+            //Use the .Clear() method to clear all item from our list box;
+            cbDisplay.Items.Clear();
+            for (int i = 0; i < students.Count; i++)
+            {
+                string firstName = students[i].FirstName;
+                string lastName = students[i].LastName;
+                string fullName = firstName + " " + lastName;
+                cbDisplay.Items.Add(fullName);               
+            }
+        }
+        public void Preload()
+        {
+            Student student = new Student("Meili", "Zheng", 90.05, 90.88);
+            students.Add(student);
+            students.Add(new Student("Hannah", "Angel", 100, 100));
+            students.Add(new Student("Dylan", "Register", 101, 99));
+            students.Add(new Student("Kris", "Taniguchi", 100, 100));
+        }
+
+        private void btnDisplayStudent_Click(object sender, RoutedEventArgs e)
+        {
+            //you can access the select item, by using .selectindex.
+            int selectindex = lbDisplay.SelectedIndex;
+            if (selectindex < 0)
+            {
+                MessageBox.Show("Please select a name from the list box");
+            }
+            else
+            {
+                DisplayStudentInformation(selectedsudent);
+            }
+            
+        }
+        public void DisplayStudentInformation(Student student)
+        {
+
+            txtFirstName.Text = student.FirstName;
+            txtLastName.Text = student.LastName;
+            txtCSIGrade.Text = student.CSIgrade.ToString();
+            txtGenED.Text = student.GenEdGrade.ToString();
+        }
+
+        private void btnAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string fName = txtFirstName.Text;
+            string lName = txtLastName.Text;
+            string CSI = txtCSIGrade.Text;
+            string GenED = txtGenED.Text;
+            double CSIgrade = double.Parse(txtGenED.Text);
+            double GenEdGrade = double.Parse(txtGenED.Text);
+            students.Add(new Student(fName, lName, CSIgrade, GenEdGrade));
+            DisplayToListBox();
+        }
+
+        private void btnRemoveStudent_Click(object sender, RoutedEventArgs e)
+        {
+            //remove by index;
+            //int selectedIndex = lbDisplay.SelectedIndex;
+            //students.RemoveAt(selectedIndex);
+            //remove by object
+            int selectindex = lbDisplay.SelectedIndex;
+          
+            students.RemoveAt(selectindex);
+            DisplayToListBox();
+        }
+
+        public void DisplayUpdatedInformation(int selectedindex)
+        {
+            selectedsudent = students[selectedindex];
+            DisplayStudentInformation(selectedsudent);
+        }
+
+        private void lbDisplay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DisplayUpdatedInformation(lbDisplay.SelectedIndex);
+        }
+
+        private void cbDisplay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DisplayUpdatedInformation(cbDisplay.SelectedIndex);
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace Lecture_Example___Selection_Box___ListBox_and_Combo_Box__
+{
+    public class Student
+    {
+        //fileds
+        private string firstName;
+        private string lastName;
+        private double cSIgrade;
+        private double genEdGrade;
+
+        public Student(string firstName, string lastName, double cSIgrade, double genEdGrade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CSIgrade = cSIgrade;
+            GenEdGrade = genEdGrade;
+        }
+
+        //override our Tostring();
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + "--" + CSIgrade + "--" + GenEdGrade;
+        }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public double CSIgrade { get => cSIgrade; set => cSIgrade = value; }
+        public double GenEdGrade { get => genEdGrade; set => genEdGrade = value; }
+
+    }
+}
